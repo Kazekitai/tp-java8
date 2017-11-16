30fcf602d0605a079046116d17b1511310c6aab3
ETY0006 <ETY0006@DESKTOP-UQ85EL9>
2017-11-16 17:44:32 +0100

    TPJava8 API Stream exercice1 fini et execrice 2 en cours

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -18,30 +18,32 @@ import static org.junit.Assert.*;
  */
 public class Stream_02_Test {
 
-    @Test
-    public void test_map() throws Exception {
+	@Test
+	public void test_map() throws Exception {
 
-        List<Order> orders = new Data().getOrders();
+		List<Order> orders = new Data().getOrders();
 
-        // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = null;
+		// Trouver la liste des clients ayant déjà passés une commande
+		List<Customer> result = orders.stream().filter(p -> p.getPizzas().size() > 0).map(p -> p.getCustomer())
+				.collect(Collectors.toList());
+	
+		assertThat(result, hasSize(2));
+	}
 
-        assertThat(result, hasSize(2));
-    }
+	@Test
+	public void test_flatmap() throws Exception {
 
-    @Test
-    public void test_flatmap() throws Exception {
+		List<Order> orders = new Data().getOrders();
 
-        List<Order> orders = new Data().getOrders();
+		// TODO calculer les statistiques sur les prix des pizzas vendues
+		// TODO utiliser l'opération summaryStatistics
+		IntSummaryStatistics result = null;
+//		IntSummaryStatistics result = orders.stream().flatMap(p -> p.getPizzas()).
+//				.collect(Collectors.summarizingInt(o -> o.getPizzas().stream()));
 
-        // TODO calculer les statistiques sur les prix des pizzas vendues
-        // TODO utiliser l'opération summaryStatistics
-        IntSummaryStatistics result = null;
-
-
-        assertThat(result.getSum(), is(10900L));
-        assertThat(result.getMin(), is(1000));
-        assertThat(result.getMax(), is(1375));
-        assertThat(result.getCount(), is(9L));
-    }
+		assertThat(result.getSum(), is(10900L));
+		assertThat(result.getMin(), is(1000));
+		assertThat(result.getMax(), is(1375));
+		assertThat(result.getCount(), is(9L));
+	}
 }
